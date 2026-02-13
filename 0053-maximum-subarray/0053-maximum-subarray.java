8311c1c9fa3dfc82b13cd372b069957bdbab3f5c
Harish E <eharish3005@gmail.com>
2026-02-13 21:12:24 +0530

    Time: 1 ms (99.62%), Space: 77.1 MB (91.74%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,16 @@
-class Solution {
-    public int maxSubArray(int[] nums) {
-        int currmax=nums[0];
-        int globalmax=nums[0];
-        for(int i=1;i<nums.length;i++){
-            currmax=Math.max(nums[i],currmax+nums[i]);
-            globalmax=Math.max(currmax,globalmax);
-        }
-        return globalmax;
+      class Solution {
+        public int maxSubArray(int[] nums) {
+            int currmax=nums[0];
+            int globalmax=nums[0];
+            for(int i=1;i<nums.length;i++){
+                currmax=Math.max(nums[i],currmax+nums[i]);
+
+                globalmax=Math.max(currmax,globalmax);
+
+            }
+            return globalmax;
+            
+            
+            
+            }
     }
-}
